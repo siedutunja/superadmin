a0f484eaaaa5fe5ca0da127bcbe8fb76940e347a
Cuervo <destebancuervo08@gmail.com>
2026-02-10 21:42:48 -0500

    Actualiza cambios locales en Inicio.vue

--- src/views/Inicio.vue
+++ src/views/Inicio.vue
@@ -129,10 +129,6 @@
       iniciarVista() {
         this.escudoInstitucion = 'https://siedutunja.gov.co/home/escudo-tunja.png'
         
-
-
-
-
         this.$store.commit('set', ['nombreInstitucion', 'INSTITUCIÓN EDUCATIVA DE PRUEBAS'])
         this.$store.commit('set', ['sectorInstitucion', 1])
         this.$store.commit('set', ['nitInstitucion', '901.000.001'])
